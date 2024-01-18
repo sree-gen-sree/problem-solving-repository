b17d630b6c727617cea8120a5c2fd118f5967c6e
SREELAKSHMI A M <amsreelakshmi20@gmail.com>
2024-01-18 22:13:13 +0530

    added file 'Final Value of Variable After Performing Operations'

---
+++ Final Value of Variable After Performing Operations.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int X = 0;
+        for(int i=0; i<operations.length; i++){
+            if(operations[i].equals("++X") || (operations[i].equals("X++"))){
+                X++;
+            }else{
+                X--;
+            }
+        }
+        return  X;
+    }
+}

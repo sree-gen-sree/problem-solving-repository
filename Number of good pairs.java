8c690b9672a7e84d63d58bb54d6eb01fa4a4703d
SREELAKSHMI A M <amsreelakshmi20@gmail.com>
2024-01-18 21:40:15 +0530

    Added time and space complexity for Number of good pair problem

--- Number of good pairs.java
+++ Number of good pairs.java
@@ -13,3 +13,6 @@ class Solution {
         return count;
     }
 }
+
+/*time complexity = O(n^2) --> till outer loop n times, inner loop will run or (n-1) for 1st time, (n-2) for 2nd, till 1. Therefore, sum is (n*(n-1))/2 = n^2
+/*Space complexity = The only variables used are count and n, which do not depend on the input size. Therefore, the space complexity is O(1).
